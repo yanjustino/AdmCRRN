47c786238f58b122d627c48cd52847e3f911d3f7
Producao01 <Producao01@tec-soft.com.br>
2012-04-29 11:33:31 -0300

    Criacao do DbContext e Migracao

---
+++ AdmCRRN/Context/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using AdmCRRN.Models;
+
+namespace AdmCRRN.Context
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<CentroAdministrativo> CentrosAdministrativos { get; set; }
+        public DbSet<Entidade> Entidades { get; set; }
+        public DbSet<Membro> Membros { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
