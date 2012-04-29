93392988b25c0fa8f74093da6bd85bb7fecd9a29
Producao01 <Producao01@tec-soft.com.br>
2012-04-29 11:35:47 -0300

    Criação do Model, Controller e Views das Entidades

---
+++ AdmCRRN/Controllers/EntidadeController.cs
@@ -0,0 +1,99 @@
+﻿using System.Web.Mvc;
+using AdmCRRN.Context;
+using AdmCRRN.Models;
+using AdmCRRN.Models.Agregados;
+
+namespace AdmCRRN.Controllers
+{
+    public class EntidadeController : Controller
+    {
+        DataContext contexto = new DataContext();
+
+        public ActionResult Index()
+        {
+            return View(contexto.Entidades);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(contexto.Entidades.Find(id));
+        }
+
+        public ActionResult Create()
+        {
+            Entidade model = new Entidade();
+            model.Endereco = new Endereco();
+            //model.CentroAdministrativo = null;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Entidade model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    contexto.Entidades.Add(model);
+                    contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(contexto.Entidades.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Entidade model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    contexto.Entry(model).State = System.Data.EntityState.Modified;
+                    contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(contexto.Entidades.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Entidade model)
+        {
+            try
+            {
+                model = contexto.Entidades.Find(model.Id);
+                contexto.Entidades.Remove(model);
+                contexto.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AdmCRRN/Models/Entidade.cs
@@ -0,0 +1,30 @@
+﻿using AdmCRRN.Models.Agregados;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace AdmCRRN.Models
+{
+    public enum TipoEntidade: int
+    {
+        Pregacao,
+        Congregacao,
+        Igreja
+    }
+
+    [Table("Entidades")]
+    public class Entidade
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o Cnpj")]
+        public string Cnpj { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome da Entidade")]
+        public string Nome { get; set; }
+
+        public int Tipo { get; set; }
+        public virtual CentroAdministrativo CentroAdministrativo { get; set; }
+        public virtual Endereco Endereco { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
