5c8911c91515fe81e05789798170e3eba806a66d
Producao01 <Producao01@tec-soft.com.br>
2012-04-29 11:34:43 -0300

    Criacao do Model e Views do Endereço

---
+++ AdmCRRN/Models/Agregados/Endereco.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AdmCRRN.Models.Agregados
+{
+    [Table("Enderecos")]
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Bairro { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Uf { get; set; }
+        public string Complemento { get; set; }
+    }
+}
