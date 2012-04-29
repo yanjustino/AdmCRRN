6a073e9ebaadceb847e659437f5e86086db9e411
Producao01 <Producao01@tec-soft.com.br>
2012-04-29 11:36:02 -0300

    Criação do Model, Controller e Views dos Membros

---
+++ AdmCRRN/Controllers/MembroController.cs
@@ -0,0 +1,99 @@
+﻿using System.Web.Mvc;
+using AdmCRRN.Context;
+using AdmCRRN.Models;
+using AdmCRRN.Models.Agregados;
+
+namespace AdmCRRN.Controllers
+{
+    public class MembroController : Controller
+    {
+        DataContext contexto = new DataContext();
+
+        public ActionResult Index()
+        {
+            return View(contexto.Membros);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(contexto.Membros.Find(id));
+        }
+
+        public ActionResult Create()
+        {
+            Membro model = new Membro();
+            model.Endereco = new Endereco();
+            //model.Entidade = null;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Membro model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    contexto.Membros.Add(model);
+                    contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(contexto.Membros.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Membro model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    contexto.Entry(model).State = System.Data.EntityState.Modified;
+                    contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(contexto.Membros.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Membro model)
+        {
+            try
+            {
+                model = contexto.Membros.Find(model.Id);
+                contexto.Membros.Remove(model);
+                contexto.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AdmCRRN/Models/Membro.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AdmCRRN.Models.Agregados;
+
+namespace AdmCRRN.Models
+{
+    [Table("Membros")]
+    public class Membro
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o Cpf")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do Membro")]
+        public string Nome { get; set; }
+
+        public virtual Endereco Endereco { get; set; }
+        public virtual Entidade Entidade { get; set; }
+    }
+}
