1f46e3f46cb6593bae70b47f72d96b75ee59c001
Producao01 <Producao01@tec-soft.com.br>
2012-04-29 11:35:16 -0300

    Criacao do Model, Controller e View do Centro Administrativo

---
+++ AdmCRRN/Controllers/CentroAdministrativoController.cs
@@ -0,0 +1,98 @@
+﻿using System.Web.Mvc;
+using AdmCRRN.Context;
+using AdmCRRN.Models;
+using AdmCRRN.Models.Agregados;
+
+namespace AdmCRRN.Controllers
+{
+    public class CentroAdministrativoController : Controller
+    {
+        DataContext contexto = new DataContext();
+        
+        public ActionResult Index()
+        {
+            return View(contexto.CentrosAdministrativos);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(contexto.CentrosAdministrativos.Find(id));
+        }
+
+        public ActionResult Create()
+        {
+            CentroAdministrativo model = new CentroAdministrativo();
+            model.Endereco = new Endereco();
+            return View(model);
+        } 
+
+        [HttpPost]
+        public ActionResult Create(CentroAdministrativo model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    contexto.CentrosAdministrativos.Add(model);
+                    contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            return View(contexto.CentrosAdministrativos.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CentroAdministrativo model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    contexto.Entry(model).State = System.Data.EntityState.Modified;
+                    contexto.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(contexto.CentrosAdministrativos.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(CentroAdministrativo model)
+        {
+            try
+            {
+                model = contexto.CentrosAdministrativos.Find(model.Id);
+                contexto.CentrosAdministrativos.Remove(model);
+                contexto.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
